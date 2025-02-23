00b4b259eb35a490cf6383a5f22f593205f950f6
Oscar Gonzalez <opt3.oscar@gmail.com>
2025-02-23 21:05:37 +0100

    Version final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ import { INoticia } from './interfaces/inoticia.interface';
 })
 export class AppComponent {
   arrNoticias: INoticia[] = [
-    { titulo: "Asteroide destruirá la Tierra", imagen: "https://th.bing.com/th/id/OSK.HEROsmvbJ-emGbdV3n71rJxtB7wBdqvkdf4vJ3nti_ZgnI0?rs=1&pid=ImgDetMain", texto: "fdweawerfewqrgfewrg", fecha: "21/02/2025"},
-    { titulo: "Ultima hora de la guerra de Ucrania", imagen: "https://th.bing.com/th/id/OIP.5HJRPil-CJMidUN8ykuH_wHaEK?rs=1&pid=ImgDetMain", texto: "fdweawerfewqrgfewrg", fecha: "20/02/2025"}
+    { titulo: "El riesgo de que el asteroide 2024 YR4 choque contra la Tierra ya es el más alto jamás registrado", imagen: "https://th.bing.com/th/id/OSK.HEROsmvbJ-emGbdV3n71rJxtB7wBdqvkdf4vJ3nti_ZgnI0?rs=1&pid=ImgDetMain", texto: "El asteroide 2024 YR4, descubierto recientemente, tiene ahora un 3,1% de posibilidades de impactar contra la Tierra el 22 de diciembre de 2032, el nivel más alto jamás registrado desde que se hacen este tipo de mediciones, según los cálculos que la NASA proporcionó el martes.", fecha: "2018-03-16"},
+    { titulo: "Ultima hora de la guerra de Ucrania", imagen: "https://th.bing.com/th/id/OIP.5HJRPil-CJMidUN8ykuH_wHaEK?rs=1&pid=ImgDetMain", texto: "Con la intensificación de los ataques, especialmente con drones, y la reciente autorización de Ucrania para usar misiles de largo alcance en territorio ruso, el conflicto parece lejos de terminar.", fecha: "2018-03-16"}
   ];
 
   obtenerDatos(event: INoticia) {
